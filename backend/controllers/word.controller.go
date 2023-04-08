ef1a6f7bf09e49d0b6e4343784e5c904c8d5c1da
rsmciftci <rsmciftci@gmail.com>
2023-04-09 02:34:16 +0300

    word controller created

---
+++ backend/controllers/word.controller.go
@@ -0,0 +1,69 @@
+package controllers
+
+import (
+	"net/http"
+	"vocabulary-builder/models"
+	"vocabulary-builder/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+type WordController struct {
+	WordService services.WordService
+}
+
+func (c *WordController) SaveWord(ctx *gin.Context) {
+	var word models.Word
+	if err := ctx.ShouldBindJSON(&word); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"Message": err.Error()})
+		return
+	}
+	err := c.WordService.SaveWord(&word)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"Message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"Message": "Success"})
+}
+func (c *WordController) DeleteWordById(ctx *gin.Context) {
+	id := ctx.Param("id")
+	err := c.WordService.DeleteWordById(id)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"Message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"Message": "Success"})
+}
+
+func (c *WordController) UpdateWord(ctx *gin.Context) {
+	var word models.Word
+	if err := ctx.ShouldBindJSON(&word); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"Message": err.Error()})
+		return
+	}
+	err := c.WordService.UpdateWord(&word)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"Message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"Message": "Success"})
+}
+
+func (c *WordController) FindWord(ctx *gin.Context) {
+	word := ctx.Param("word")
+	result, err := c.WordService.FindWord(word)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"Message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, result)
+
+}
+
+func (c *WordController) RegisterUserRoutes(routerGroup *gin.RouterGroup) {
+	userRoute := routerGroup.Group("/word")
+	userRoute.POST("", c.SaveWord)
+	userRoute.DELETE("/:id", c.DeleteWordById)
+	userRoute.PUT("", c.UpdateWord)
+	userRoute.GET("/:word", c.FindWord)
+}
