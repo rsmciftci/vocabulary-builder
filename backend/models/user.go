447fb516ceb520c1383688b96ab4e7e712385032
rsmciftci <rsmciftci@gmail.com>
2023-04-09 23:33:45 +0300

    email filed made unique field

--- backend/models/user.go
+++ backend/models/user.go
@@ -9,6 +9,6 @@ type User struct {
 	Surname     *string             `json:"surname" validate:"required"`
 	DateOfBirth *primitive.DateTime `json:"date_of_birth" validate:"required,datetime"`
 	Gender      *string             `json:"gender" validate:"required"`
-	Email       *string             `json:"email" validate:"email,required"` // TODO: unique olmali, kayit sirasinda sorgula
-	Password    *string             `json:"password" `
+	Email       *string             `json:"email" bson:"_id" validate:"email,required" `
+	Password    *string             `json:"password" ` // TODO: password md5 olarak tutulmali
 }
