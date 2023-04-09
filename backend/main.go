c864f888556ba7303eacad4f654119f40bb3ddc1
rsmciftci <rsmciftci@gmail.com>
2023-04-09 21:16:14 +0300

    mongo connection

--- backend/main.go
+++ backend/main.go
@@ -3,37 +3,82 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"vocabulary-builder/controllers"
+	"vocabulary-builder/services"
 
-	"go.mongodb.org/mongo-driver/bson"
+	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // Replace the placeholder with your Atlas connection string
 const uri = "mongodb://localhost:27017"
 
-func main() {
+var (
+	server             *gin.Engine
+	us                 services.UserService
+	uc                 controllers.UserController
+	uws                services.UserWordService
+	uwc                controllers.UserWordController
+	ws                 services.WordService
+	wc                 controllers.WordController
+	vs                 services.VideoService
+	vc                 controllers.VideoController
+	ctx                context.Context
+	userCollection     *mongo.Collection
+	userWordCollection *mongo.Collection
+	wordCollection     *mongo.Collection
+	videoCollection    *mongo.Collection
 
-	// Use the SetServerAPIOptions() method to set the Stable API version to 1
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	mongoClient *mongo.Client
+	err         error
+)
 
-	// Create a new client and connect to the server
-	client, err := mongo.Connect(context.TODO(), opts)
+func init() {
+	ctx = context.TODO()
 
+	mongoConnection := options.Client().ApplyURI(uri)
+	mongoClient, err := mongo.Connect(ctx, mongoConnection)
 	if err != nil {
-		panic(err)
+		log.Fatal("error while connecting to mongo: ", err)
 	}
-	defer func() {
-		if err = client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
+	err = mongoClient.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal("error while trying to ping mongo: ", err)
+	}
 
-	// Send a ping to confirm a successful connection
-	var result bson.M
-	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
-		panic(err)
-	}
-	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+	fmt.Println("mongo connection established")
+
+	userCollection = mongoClient.Database("vocabularybuilder").Collection("users")
+	us = services.NewUserService(userCollection, ctx)
+	uc = controllers.NewUserController(us)
+
+	userWordCollection = mongoClient.Database("vocabularybuilder").Collection("userwords")
+	uws = services.NewUserWordService(userWordCollection, ctx)
+	uwc = controllers.NewUserWordController(uws)
+
+	wordCollection = mongoClient.Database("vocabularybuilder").Collection("words")
+	ws = services.NewWordService(wordCollection, ctx)
+	wc = controllers.NewWordController(ws)
+
+	videoCollection = mongoClient.Database("vocabularybuilder").Collection("videos")
+	vs = services.NewVideoService(videoCollection, ctx)
+	vc = controllers.NewVideoController(vs)
+
+	server = gin.Default()
+}
+
+func main() {
+	defer mongoClient.Disconnect(ctx)
+
+	basepath := server.Group("/v1")
+	uc.RegisterUserRoutes(basepath)
+	uwc.RegisterUserRoutes(basepath)
+	wc.RegisterUserRoutes(basepath)
+	vc.RegisterVideoRoutes(basepath)
+
+	log.Fatal(server.Run(":9090"))
+
 }
