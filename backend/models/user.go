c5330bfe97099e80deefa4bae61ff0fbd452ae44
rsmciftci <rsmciftci@gmail.com>
2023-04-10 01:12:06 +0300

    todos added

--- backend/models/user.go
+++ backend/models/user.go
@@ -3,8 +3,8 @@ package models
 type User struct {
 	Name        *string `binding:"required"`
 	Surname     *string `binding:"required"`
-	DateOfBirth *string `binding:"required"` // TODO: current time or past
-	Gender      *string `binding:"required"` //TODO: "male" or "female"
+	DateOfBirth *string `binding:"required"` // TODO: validate current time or past
+	Gender      *string `binding:"required"` //TODO: validate gender
 	Email       *string `bson:"_id" binding:"required,email"`
 	Password    *string `binding:"required,min=6"` //  TODO: password md5 olarak tutulmali
 }
