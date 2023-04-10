aacd80ba4065eabd8217590ff9b31d87b4c91f55
rsmciftci <rsmciftci@gmail.com>
2023-04-11 01:23:56 +0300

    not required anymore

--- backend/dto/userworddto.go
+++
@@ -1,8 +0,0 @@
-package dto
-
-type UserWordDto struct {
-	ObjectId string `json:"object" validate:"required"`
-	UserId   string `json:"user_id" validate:"required" `
-	WordId   string `json:"word_id" validate:"required"`
-	Learned  *bool  `json:"learned" validate:"required"`
-}
